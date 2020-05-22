ca3e63ca356138577c837b89eb07a9bb477b2980
nhat499 <nhat499@uw.edu>
2020-05-21 21:21:24 -0700

    test

---
+++ scripts/sumInfo.R
@@ -0,0 +1,12 @@
+data <- read.csv("../data/google_new.csv", stringsAsFactors = FALSE)
+library(dplyr)
+
+most_death_country <- data %>%
+  group_by(country) %>%
+  summarise(total_death = sum(fatalities)) %>%
+  filter(total_death == max(total_death))
+
+most_cases_country <- data %>%
+  group_by(country) %>%
+  summarise(total_case = sum(total_cases)) %>%
+  filter(total_case == max(total_case))
