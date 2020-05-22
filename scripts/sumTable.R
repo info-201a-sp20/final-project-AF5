d008459926359903d8039841fb3bf329af7103d9
nhat499 <nhat499@uw.edu>
2020-05-21 22:59:13 -0700

    sum table

---
+++ scripts/sumTable.R
@@ -0,0 +1,11 @@
+data <- read.csv("../data/google_new.csv", stringsAsFactors = FALSE)
+library(knitr)
+
+
+summary <- function(data) {
+  table <- kable(data %>%
+    group_by(country) %>%
+    summarise(total_death = sum(fatalities), total_cases = sum(total_cases)))
+  return(table)
+}
+
