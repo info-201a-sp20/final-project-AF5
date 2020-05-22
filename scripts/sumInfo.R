d43810c3c970bad8e29e1e2dfb52805f2c2c89b9
nhat499 <nhat499@uw.edu>
2020-05-21 22:15:25 -0700

    nhat

--- scripts/sumInfo.R
+++ scripts/sumInfo.R
@@ -3,10 +3,49 @@ library(dplyr)
 
 most_death_country <- data %>%
   group_by(country) %>%
-  summarise(total_death = sum(fatalities)) %>%
+  summarise(total_death = sum(fatalities),
+            grocery_avg = mean(grocery_pharmacy),
+            park_avg = mean(parks),
+            residential_avg = mean(residential),
+            retail_recreation_avg = mean(retail_recreation),
+            transit_avg = mean(transit_stations),
+            workplaces_avg = mean(workplaces)
+            ) %>%
   filter(total_death == max(total_death))
 
 most_cases_country <- data %>%
   group_by(country) %>%
-  summarise(total_case = sum(total_cases)) %>%
+  summarise(total_case = sum(total_cases),
+            grocery_avg = mean(grocery_pharmacy),
+            park_avg = mean(parks),
+            residential_avg = mean(residential),
+            retail_recreation_avg = mean(retail_recreation),
+            transit_avg = mean(transit_stations),
+            workplaces_avg = mean(workplaces)
+            ) %>%
   filter(total_case == max(total_case))
+
+least_cases_country <- data %>%
+  group_by(country) %>%
+  summarise(total_case = sum(total_cases),
+            grocery_avg = mean(grocery_pharmacy),
+            park_avg = mean(parks),
+            residential_avg = mean(residential),
+            retail_recreation_avg = mean(retail_recreation),
+            transit_avg = mean(transit_stations),
+            workplaces_avg = mean(workplaces)
+            ) %>%
+  filter(total_case == min(total_case))
+
+least_death_country <- data %>%
+  group_by(country) %>%
+  summarise(total_death = sum(fatalities),
+            grocery_avg = mean(grocery_pharmacy),
+            park_avg = mean(parks),
+            residential_avg = mean(residential),
+            retail_recreation_avg = mean(retail_recreation),
+            transit_avg = mean(transit_stations),
+            workplaces_avg = mean(workplaces)
+            ) %>%
+  filter(total_death == min(total_death))
+
