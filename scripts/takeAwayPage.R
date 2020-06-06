473eb67c5fae54e76c50558e23640a2fd7c6664e
nhat499 <nhat499@uw.edu>
2020-06-05 18:13:27 -0700

    2nd takeaway

--- scripts/takeAwayPage.R
+++ scripts/takeAwayPage.R
@@ -1,6 +1,7 @@
 takeAwayPage <- tabPanel(
-  "Take Aways",
+  "Takeaways",
   fluidPage(
+    h1("Takeaways:"),
     h2("1st take away"),
     p(" All the countries have responded differently to the virus,",
       "and the change of the mobility trend resulted accordingly.",
@@ -14,7 +15,24 @@ takeAwayPage <- tabPanel(
       "the virus compared to western countries such as the U.S., because they",
       "were affected by the virus earlier than the U.S."),
     h2("2nd take away"),
-    p("something soemthing..."),
-    h3("3rd take away")
+    p("From the most impacted page's map, we can see which countries were most impacted at which point",
+    "in this critical time period. Using this graph, we can see that Italy was the",
+    "most impacted in terms of casualties with 15887 total casualties in this",
+    "timeframe. However, the US was the most impacted in terms of cases with 336912",
+    "cases. Going through the timeframe, we can see that the outbreak in western",
+    "countries notably begins to spark in Italy, and later affecting neighboring",
+    "countries. The US however beats Italy in confirmed cases in the middle of this",
+    "timeframe. Between March 25th and March 26th, Italy gains 6203 new confirmed",
+    "cases, while the US gains 18058 new cases, almost three times as much. The US,",
+    "continues to increase in cases in a much more rapid pace than Italy or any other",
+    "country in the world for the remaining time frame. Some broader implications that",
+    "we can draw from this data is that countries were hit at different times, and in",
+    "some cases like the US, spiraled much more out of control than other places because",
+    "of how the country dealt with the situation. Comparing the US and South Korea again,",
+    "South Korea's cases multiplied by around 1.2x from March 20th to April 5th, while the",
+    "US' cases multiplied around 17.7x. Implications that can be drawn here is that countries",
+    "handled the virus differently and suffered more or less cases depending on their response,",
+    "in this timeframe."),
+    h2("3rd take away")
   )
 )
